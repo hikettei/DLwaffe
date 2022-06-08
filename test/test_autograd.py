8ca42e68d5a85fa01e74f588154057525243ba2c
hikettei <hikettei@gmail.com>
2022-06-09 04:04:17 +0900

    test

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -4,8 +4,21 @@ import torch
 
 device = wf.get_device("device:1")
 
-a = wf.Tensor(2, device=device)
-b = wf.Tensor(2, device=device)
-y = a + b
+a = wf.Tensor(1.0, device=device)
+b = wf.Tensor(2.0, device=device)
+#c = wf.Tensor(3, device=device)
 
-print(y)
+#z = wf.log(a * b) * wf.sin(a * b)
+z = wf.sin(a * b)
+z.backward()
+
+print(a.grad)
+print(b.grad)
+
+a = torch.tensor(1.0, requires_grad=True)
+b = torch.tensor(2.0, requires_grad=True)
+z = torch.sin(a * b)
+z.backward()
+
+print(a.grad)
+print(b.grad)
