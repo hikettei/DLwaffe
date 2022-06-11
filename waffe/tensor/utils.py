1e9a79856ab62987a4dd38aee441ca9b11eb0e77
hikettei <hikettei@gmail.com>
2022-06-11 18:06:58 +0900

    autograd (but inaccurate...)

--- waffe/tensor/utils.py
+++ waffe/tensor/utils.py
@@ -18,7 +18,7 @@ def sin(tensor, require_grad=True):
 
 	cpu_earr = np.empty((N, M), dtype=tensor.device.DTYPE)
 	resbuff  = cl.Buffer(tensor.device.ctx, cl.mem_flags.READ_WRITE, size=cpu_earr.nbytes)
-	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor)
+	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor, is_constant=False)
 
 	event = tensor.device.prg.matsin(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
 	cl.wait_for_events([event, ])
@@ -31,21 +31,21 @@ def sin(tensor, require_grad=True):
 def cos(tensor, require_grad=True):
 	def cos_backward(tensor):
 		def _cos_backward(x):
-			return -wf.sin(tensor)
+			return wf.sin(tensor, require_grad=False) * -1
 		return _cos_backward
 	M = np.int32(tensor.dim()[0])
 	N = np.int32(tensor.dim()[1])
 
 	cpu_earr = np.empty((N, M), dtype=tensor.device.DTYPE)
 	resbuff  = cl.Buffer(tensor.device.ctx, cl.mem_flags.READ_WRITE, size=cpu_earr.nbytes)
-	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor)
+	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor, is_constant=False)
 
 	event = tensor.device.prg.matcos(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
 	cl.wait_for_events([event, ])
 
 	if require_grad:
-		wf.register_derivative(res, cos_backward())
-		wf.register_variables(res, tensor)
+		wf.register_derivative(res, cos_backward(tensor), tensor)
+		wf.register_variables(res, tensor.variables)
 	res.sync()
 	return res
 
@@ -59,7 +59,7 @@ def log(tensor, require_grad=True):
 
 	cpu_earr = np.empty((N, M), dtype=tensor.device.DTYPE)
 	resbuff  = cl.Buffer(tensor.device.ctx, cl.mem_flags.READ_WRITE, size=cpu_earr.nbytes)
-	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor)
+	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor, is_constant=False)
 
 	event = tensor.device.prg.matlog(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
 	cl.wait_for_events([event, ])
