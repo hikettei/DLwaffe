7e699288667d7a5103bafd71f6fc1b00e2d26910
hikettei <hikettei@gmail.com>
2022-06-11 18:27:09 +0900

    new test

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -8,20 +8,34 @@ a = wf.Tensor(2.0, device=device)
 b = wf.Tensor(9.0, device=device)
 c = wf.Tensor(3.0, device=device)
 
-#z = wf.log(a * b) * wf.sin(a * b)
-
-z = wf.sin(a * b) * wf.cos(a * b)
-# {a * wf.cos(a * b)} * 1 + 0 * wf.sin(a * b)
-# 
+z = wf.sin(wf.sin(a * b)) * wf.cos(a * b) + c
 z.backward()
+
+print("==Test1:========")
 print(a.grad)
 print(b.grad)
+print(c.grad)
+
+z = wf.sin(wf.sin(a * b) + c)
+z.backward()
+print("==Test2:========")
+print(a.grad)
+print(b.grad)
+print(c.grad)
 
 a = torch.tensor(2.0, requires_grad=True)
 b = torch.tensor(9.0, requires_grad=True)
 c = torch.tensor(3.0, requires_grad=True)
-z = torch.sin(a * b) * torch.cos(a * b)
+z = torch.sin(torch.sin(a * b)) * torch.cos(a * b) + c
 z.backward()
-
+print("==Test1:========")
 print(a.grad)
 print(b.grad)
+print(c.grad)
+
+z = torch.sin(torch.sin(a * b) + c)
+z.backward()
+print("==Test2:========")
+print(a.grad)
+print(b.grad)
+print(c.grad)
