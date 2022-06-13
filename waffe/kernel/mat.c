9fff917a5cfb41e8e7d911fb85cf6d7af6bcea6f
hikettei <hikettei@gmail.com>
2022-06-13 15:29:23 +0900

    sigmoid etc...

--- waffe/kernel/mat.c
+++ waffe/kernel/mat.c
@@ -288,4 +288,61 @@ __kernel void matlog(const int M,
 }
 
 
+__kernel void sigmoid(const int M,
+	const int N,
+	__global floatX* A,
+	__global floatX* B)
+{
+	// // Thread identifiers
+	const int tx = get_local_id(0);
+	const int ty = get_local_id(1);
+	const int ID0 = get_group_id(0) * TS + tx; // 0..M
+	const int ID1 = get_group_id(1) * TS + ty; // 0..N
+
+	// Set-up the local memory for shuffling
+	__local floatX bufferA[TS][TS];
+
+	// Swap the x and y coordinates to perform the rotation (coalesced)
+	if (ID0 < M && ID1 < N) {
+		bufferA[tx][ty] = A[ID1 * M + ID0];
+	}
+
+	// Synchronise all threads
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	// Store the sum result (coalesced)
+	if (ID0 < M && ID1 < N) {
+		B[ID1 * M + ID0] = 1 / (1 + exp(-bufferA[tx][ty]));
+	}
+}
+
+__kernel void dsigmoid(const int M,
+	const int N,
+	__global floatX* A,
+	__global floatX* B)
+{
+	// // Thread identifiers
+	const int tx = get_local_id(0);
+	const int ty = get_local_id(1);
+	const int ID0 = get_group_id(0) * TS + tx; // 0..M
+	const int ID1 = get_group_id(1) * TS + ty; // 0..N
+
+	// Set-up the local memory for shuffling
+	__local floatX bufferA[TS][TS];
+
+	// Swap the x and y coordinates to perform the rotation (coalesced)
+	if (ID0 < M && ID1 < N) {
+		bufferA[tx][ty] = A[ID1 * M + ID0];
+	}
+
+	// Synchronise all threads
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	// Store the sum result (coalesced)
+	if (ID0 < M && ID1 < N) {
+		B[ID1 * M + ID0] = bufferA[tx][ty] * (1 - bufferA[tx][ty]);
+	}
+}
+
+
 
--- waffe/tensor/utils.py
+++ waffe/tensor/utils.py
@@ -3,22 +3,25 @@ import waffe as wf
 import pyopencl as cl
 import pyopencl.array as clarr
 import numpy as np
-from functools import singledispatch
 
 # マクロ使いてぇ〜〜〜
 
-def sin(tensor, require_grad=True):
-	def sin_backward(tensor):
-		def _sin_backward(x):
-			return wf.cos(tensor, require_grad=False)
-		return _sin_backward
-
+def create_res_buffer(tensor):
 	M = np.int32(tensor.dim()[0])
 	N = np.int32(tensor.dim()[1])
 
 	cpu_earr = np.empty((N, M), dtype=tensor.device.DTYPE)
 	resbuff  = cl.Buffer(tensor.device.ctx, cl.mem_flags.READ_WRITE, size=cpu_earr.nbytes)
 	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor, is_constant=False)
+
+	return M, N, res
+def sin(tensor, require_grad=True):
+	def sin_backward(tensor):
+		def _sin_backward(x):
+			return wf.cos(tensor, require_grad=False)
+		return _sin_backward
+
+	M, N, res = create_res_buffer(tensor)
 
 	event = tensor.device.prg.matsin(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
 	cl.wait_for_events([event, ])
@@ -33,12 +36,8 @@ def cos(tensor, require_grad=True):
 		def _cos_backward(x):
 			return wf.sin(tensor, require_grad=False) * -1
 		return _cos_backward
-	M = np.int32(tensor.dim()[0])
-	N = np.int32(tensor.dim()[1])
 
-	cpu_earr = np.empty((N, M), dtype=tensor.device.DTYPE)
-	resbuff  = cl.Buffer(tensor.device.ctx, cl.mem_flags.READ_WRITE, size=cpu_earr.nbytes)
-	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor, is_constant=False)
+	M, N, res = create_res_buffer(tensor)
 
 	event = tensor.device.prg.matcos(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
 	cl.wait_for_events([event, ])
@@ -54,15 +53,37 @@ def log(tensor, require_grad=True):
 		def _log_backward(x):
 			return 1 / tensor.detach()
 		return _log_backward
-	M = np.int32(tensor.dim()[0])
-	N = np.int32(tensor.dim()[1])
 
-	cpu_earr = np.empty((N, M), dtype=tensor.device.DTYPE)
-	resbuff  = cl.Buffer(tensor.device.ctx, cl.mem_flags.READ_WRITE, size=cpu_earr.nbytes)
-	res      = wf.Tensor(cpu_earr, device=tensor.device, x_buf=resbuff, extend=tensor, is_constant=False)
+	M, N, res = create_res_buffer(tensor)
 
 	event = tensor.device.prg.matlog(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
 	cl.wait_for_events([event, ])
+
+	if require_grad:
+		wf.register_derivative(res, log_backward(tensor), tensor)
+		wf.register_variables(res, tensor.variables)
 	res.sync()
 
 	return res
+
+def sigmoid(tensor, require_grad=True):
+	def sigmoid_backward(tensor):
+		def _sigmoid_backward(x):
+			M, N, res = create_res_buffer(tensor)
+			event = tensor.device.prg.dsigmoid(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
+			cl.wait_for_events([event, ])
+			return res
+		return _sigmoid_backward
+
+	M, N, res = create_res_buffer(tensor)
+
+	event = tensor.device.prg.sigmoid(tensor.device.queue, (1,), None, M, N, tensor.x_buf, res.x_buf)
+	cl.wait_for_events([event, ])
+
+	if require_grad:
+		wf.register_derivative(res, sigmoid_backward(tensor), tensor)
+		wf.register_variables(res, tensor.variables)
+	res.sync()
+
+	return res
+
