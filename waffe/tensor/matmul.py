f67b7a4c55fb92d07a195a79f111cb0b6e19c9e9
hikettei <hikettei@gmail.com>
2022-06-27 14:57:21 +0900

    dot product backwards:

--- waffe/tensor/matmul.py
+++ waffe/tensor/matmul.py
@@ -6,8 +6,35 @@ import pyopencl as cl
 import pyopencl.array as clarr
 
 
+def _DotProductBackward(L, X, Y):
+	def DotProductBackward(g):
+		if g == X:
+			return (X @ L / (X.transpose() @ X)).no_grad()
+		elif g == Y:
+			return (Y @ L / (Y.transpose() @ Y)).no_grad()
+		else:
+			Exception("")
+			
+	return DotProductBackward
+
 def _dot_product(A, B):
-	pass
+	assert A.dim()[0] == A.dim()[0], ""
+	M = np.int32(A.dim()[0])
+	N = np.int32(A.dim()[1])
+
+	cpu_earr = np.empty((M, N), dtype=A.device.DTYPE)
+	resbuff  = cl.Buffer(A.device.ctx, cl.mem_flags.READ_WRITE, size=cpu_earr.nbytes)
+	res      = wf.Tensor(cpu_earr, device=A.device, x_buf=resbuff, extend=A, is_constant=False)
+
+	gsize = (int(M), int(N))
+	lsize = (int(A.device.TS), int(A.device.TS))
+
+	event = A.device.prg.matdot(A.device.queue, gsize, lsize, M, N, A.x_buf, B.x_buf, res.x_buf)
+	cl.wait_for_events([event, ])
+
+	wf.register_derivative(res, _DotProductBackward(res, A, B), A, B)
+	wf.register_variables(res, [A, B])
+	return res
 
 def _matrix_vector_product(A, B):
 	pass
@@ -26,7 +53,6 @@ def _MMProductBackward(L, X, Y):
 				return Y.transpose() @ g.grad
 		else:
 			Exception("")
-
 	return MMProductBackward
 
 def _matrix_matrix_product(A, B):
